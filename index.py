c48d25aed1dafb5ff57c5e3e83f715bd811c121b
Norman Tang <norman@normstorm.com>
2021-05-02 10:55:05 -0500

    added logic to test for m:null attribute

--- index.py
+++ index.py
@@ -4,6 +4,11 @@ import requests
 from xml.dom import minidom # https://docs.python.org/3/library/xml.dom.minidom.html
 
 #########################################################
+# https://stackoverflow.com/questions/22586286/python-is-there-an-equivalent-of-mid-right-and-left-from-basic
+#########################################################
+def left(s, amount):    
+    return s[:amount]
+#########################################################
 # XML source: https://www.treasury.gov/resource-center/data-chart-center/interest-rates/pages/textview.aspx?data=yield
 #########################################################
 class Treasury:        
@@ -13,8 +18,8 @@ class Treasury:
     def getTreasuryXML(self):
   
         # url of rss feed
-       # url = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData' # all-time, rates SI
-        url = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%204%20and%20year(NEW_DATE)%20eq%202021'
+        url = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData' # all-time, rates SI
+  #      url = 'https://data.treasury.gov/feed.svc/DailyTreasuryYieldCurveRateData?$filter=month(NEW_DATE)%20eq%204%20and%20year(NEW_DATE)%20eq%202021'
   
         # creating HTTP response object from given url
         resp = requests.get(url)
@@ -39,11 +44,19 @@ class Treasury:
                 for x in range(1,elem.childNodes[13].childNodes[1].childNodes.length):
                     if (elem.childNodes[13].childNodes[1].childNodes[x].nodeType == elem.ELEMENT_NODE):
                        # print(str(x) + ' ' + elem.childNodes[13].childNodes[1].childNodes[x].tagName)
-                        if (elem.childNodes[13].childNodes[1].childNodes[x].tagName == "d:NEW_DATE"):
-                            print('date: ' + str(elem.childNodes[13].childNodes[1].childNodes[x].firstChild.data))
-                        elif (elem.childNodes[13].childNodes[1].childNodes[x].tagName == "d:BC_1MONTH"):
+                        mytag= elem.childNodes[13].childNodes[1].childNodes[x].tagName
+                        if (mytag == "d:NEW_DATE"):
+                            tsyDate = elem.childNodes[13].childNodes[1].childNodes[x].firstChild.data
+
+                        if (left(mytag,5)=="d:BC_"):
+                            if(elem.childNodes[13].childNodes[1].childNodes[x].hasAttribute('m:null') and elem.childNodes[13].childNodes[1].childNodes[x].getAttribute('m:null') == 'true'):
+                                    print(tsyDate + ' ' + mytag +': true')
+                                #print(elem.childNodes[13].childNodes[1].childNodes[x].getAttribute('m:null'))
+                                    #print('null')
+                        """
+                        elif (mytag == "d:BC_1MONTH"):
                             print('  1 mo: ' + str(elem.childNodes[13].childNodes[1].childNodes[x].firstChild.data))
-                        elif (elem.childNodes[13].childNodes[1].childNodes[x].tagName == "d:BC_2MONTH"):
+                        elif (mytag == "d:BC_2MONTH"):
                             print('  2 mo: ' + str(elem.childNodes[13].childNodes[1].childNodes[x].firstChild.data))
                         elif (elem.childNodes[13].childNodes[1].childNodes[x].tagName == "d:BC_3MONTH"):
                             print('  3 mo: ' + str(elem.childNodes[13].childNodes[1].childNodes[x].firstChild.data))
@@ -65,7 +78,7 @@ class Treasury:
                             print('  20Y: ' + str(elem.childNodes[13].childNodes[1].childNodes[x].firstChild.data))
                         elif (elem.childNodes[13].childNodes[1].childNodes[x].tagName == "d:BC_30YEAR"):
                             print('  30Y: ' + str(elem.childNodes[13].childNodes[1].childNodes[x].firstChild.data))
-
+                        """
 
 
 
